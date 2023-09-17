8ee34e2e7fc0f2f42813c417443da0065e4c8ffb
Fexsc <145291523+CoroaSafado@users.noreply.github.com>
2023-09-17 20:44:16 -0300

    Veiculos.java

---
+++ projeto/Classeprin/src/ProjetoVeiculosEstagiario/Veiculos.java
@@ -0,0 +1,30 @@
+package ProjetoVeiculosEstagiario;
+import java.time.LocalDate;
+import java.util.ArrayList;
+        abstract class Veiculos {
+
+    protected int codigo;
+    public String nome;
+    public String modelo;
+    public String cor;
+    public String chassi;
+    public String placa;
+    public String anoMOdelo;
+    public String anoFabricacao;
+    public String eixos;
+    public double pesoLiquido;
+    public String pesoTotal;
+    public int assentos; 
+    public int portas;
+    public String fabricante; 
+    public double potencia;
+    public String tipoCombustivel;
+    public String codigoRenavam;
+    public String nomeProprietario;
+    public String estadoUF;
+    public String tipoveiculo;
+    public int qtdeMotores;
+    public int qtdeOcupantes;
+    
+}
+
